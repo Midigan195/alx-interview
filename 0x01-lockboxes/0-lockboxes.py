915393d355b2d1fd6310799d27893137d359986d
Liam Bowers <liambowers718@gmail.com>
2024-03-11 21:20:38 -0700

    Updated algorith to use a loop

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,14 +8,14 @@ def canUnlockAll(boxes):
     """
     This function checks that every item in the list can be visited.
     """
-    visited = set()
-
-    def depth_search(box_index):
-        visited.add(box_index)
-        for key in boxes[box_index]:
-            if key not in visited:
-                depth_search(key)
-
-    depth_search(0)
-
-    return len(visited) == len(boxes)
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
